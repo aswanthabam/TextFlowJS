ccdcacf2bccaf5961ef3576fdce536beccfb8046
avc-tech <avctech.official@gmail.com>
2022-07-07 17:52:29 +0530

    h

--- textWrap.js
+++ textWrap.js
@@ -1,170 +1,150 @@
-function initWrap(text = null,elem = null,WorkingWith = 0){
-    if(text != null) this.currentText = text;
-    else this.currentText = null;
-    if(elem != null) this.currentElement = elem;
-    else this.currentElement = null;
-    this.SetClassName = "texter";
-    //change default class name
-    
-    // Getting the classes if exists
-    this.texter = document.getElementsByClassName(this.SetClassName);
-    // Creating the new elemnts and setting id
-    this.cursor = document.createElement("p");
-    this.cursor.className += "abamCursor1";
-    this.container = document.createElement("div");
-    this.container.className += "abamContainer1";
-    this.textContainer = document.createElement("span");
-    this.textContainer.className += "abamtextContainer1";
-    // For multiple work counting
-    this.multiI = WorkingWith;
-    // Adding the stylsheet to header
-    var link = document.createElement('link');
-    link.rel = 'stylesheet';  
-    link.type = 'text/css'; 
-    link.href = 'https://cdn.jsdelivr.net/gh/aswanthabam/TextFlowJS@main/style.css';
-    document.getElementsByTagName('HEAD')[0].appendChild(link);
-    // Declaring loopable and content variables
-    this.content;
-    this.i= 0; this.m = 0;
-    // Screen width
-    this.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    this.finished = false;
-    // Some variables which store the styling details about the code these can be changed from outside of the code to customize
-    this.cursorHeight = null;
-    this.blinkFrequency = 200;
-    this.dir = "";
-    this.minSpeed = 50;
-    this.maxSpeed = 100;
-    this.breakPoint = null;
-    this.breakTime = 1000;
-    this.breakKey = null;
-    this.wrapTextColor = "#000000";
-    this.CursorBackgroundColor = "#000000";
-    this.wrapFontFamily = null;
-    this.cursorWidth = 5;
-    this.wrapTextSize = null;
-    if(this.currentElement == null)
-        this.Elements = this.fetchElements();
+class avcWrap
+{
+	constructor()
+	{
+		this.wraps = [];
+		this.elems = [];
+		this.lineHeight = 20;
+		// Recommended variables to be changed
+		this.blinkFrequency = 1;
+		this.textColor = "#121212";
+		this.cursorColor = "#12121250";
+		this.cursorWidth = 7;
+		this.duration = 2000;
+		this.offset = 20;
+		this.cursorVisible = true;
+	}
+	start(duration = null)
+	{
+		if(duration != null) this.duration = duration;
+		for(let i = 0;i < this.wraps.length;i++)
+		{
+			this.wraps[i].getDurations();
+			this.wraps[i].start();
+		}
+	}
 }
-// Starting wrapper you can give text and element variables(optional) if not given it will be used from the className "texter"
-initWrap.prototype.setWrap = function(text = null,elem = null) {
-    if(text == null && this.currentText != null) text = this.currentText;
-    if(this.currentElement != null) elem = this.currentElement;
-        if(elem == null){
-            this.multi();
-        }else{
-        if(this.wrapTextSize == null){
-            this.wrapTextSize = elem.getBoundingClientRect().height;
-        }
-        elem.style.direction = this.dir;
-        this.cursor.style.width = this.cursorWidth + "px";
-        if(this.cursorHeight != null){
-            this.cursor.style.height = this.cursorHeight + "px";
-        }else{
-            this.cursor.style.height = this.wrapTextSize + "px";
-        }
-        this.cursor.style.background = this.CursorBackgroundColor;
-        
-        this.container.width = "100%";
-        this.container.style.display = "inline-block";
-        this.textContainer.style.direction = this.dir;
-        this.textContainer.style.color = this.wrapTextColor;
-        this.textContainer.style.fontSize = this.wrapTextSize + "px";
-        this.textContainer.style.fontFamily = this.wrapFontFamily;
-        this.container.appendChild(this.textContainer);
-        this.container.appendChild(this.cursor);
-        this.content = elem.textContent;
-        elem.textContent = "";
-        elem.appendChild(this.container);
-        this.elem2 = elem;
-        this.elem = this.textContainer;
-        
-        if (text != null && String(text).length > 1){
-            this.content = text;
-        }
-        this.elem.textContent = "";
-        this.counter = this.getRandom(this.minSpeed,this.maxSpeed);
-        this.i = 0;
-        this.myvar = setTimeout(this.setTextAbamTech,this.counter,this);
-        this.blinker = setTimeout(this.blinkCursor,this.blinkFrequency,this);
-        }
-    return 0;
-};
-initWrap.prototype.fetchElements = function(){
-    m = [];
-    for(var i = 0;i<this.texter.length;i++){
-       newObj = new initWrap(null,this.texter[i],i+1);
-       
-       m.push(newObj);
-    }
-    return m;
-};
-initWrap.prototype.multi = function(){
-    if(this.Elements.length == 1){
-        console.log(this.cloneWrap(this.Elements[0],this));
-        this.Elements[0].setWrap();
+class avcTextWrap
+{
+	constructor(elem)
+	{
+		this.elem = elem;
+		this.mwrap = null;
+		this.container = null;
+		this.text = "";
+		this.counter = 0;
+		this.minSpeed = 0;
+		this.maxSpeed = 0;
+		
+		if(this.elem == null) return 0;
+	}
+	start()
+	{
+		this.text = this.container.textContent;
+		this.elem.style.setProperty("--avc-textwrap-line-height",this.lineHeight+"px");
+		this.elem.style.setProperty("--avc-textwrap-text-color",this.mwrap.textColor);
+		this.elem.style.setProperty("--avc-textwrap-cursor-color",this.mwrap.cursorColor);
+		this.elem.style.setProperty("--avc-textwrap-cursor-width",this.mwrap.cursorWidth + "px");
+		if(!this.mwrap.cursorVisible)
+			this.container.classList.add("avc-textwrap-cursor-invisible");
+		//	this.current = 10;
+		this.container.textContent = "";
+		setTimeout(this.nextText,this.getRandom(this),this);
+		
+	}
+	nextText(This)
+	{
+		if(This.counter > This.text.length-1) return;
+		This.container.textContent += This.text[This.counter];
+		This.counter += 1;
+		setTimeout(This.nextText, This.getRandom(This),This);
+		
+	}
+	getRandom(This){
+		return Math.floor(Math.random()*(This.maxSpeed-This.minSpeed+1)) + This.minSpeed;
+		
+	}
+	wrap()
+	{
+		this.text = this.elem.textContent;
+		this.getDurations();
+		this.elem.textContent = "l";
+		this.lineHeight = this.elem.getBoundingClientRect().height;
+		this.elem.textContent = "";
+		this.elem.style.setProperty("--avc-textwrap-line-height",this.lineHeight+"px");
+		this.elem.style.setProperty("--avc-textwrap-text-color",this.mwrap.textColor);
+		this.elem.style.setProperty("--avc-textwrap-cursor-color",this.mwrap.cursorColor);
+		this.elem.style.setProperty("--avc-textwrap-cursor-width",this.mwrap.cursorWidth + "px");
+	
+		this.elem.classList.add(".avc-textwrap-container");
+		
+		this.container = document.createElement("span");
+		
+		if(!this.mwrap.cursorVisible)
+			this.container.classList.add("avc-textwrap-cursor-invisible");
+		this.container.classList.add("avc-textwrap-text-container");
+		
+		this.container.textContent = this.text;
+	
+		this.elem.appendChild(this.container);
+		
+	}
+	getDurations(text = null)
+	{
+		if(text == null) text = this.text;
+		this.minSpeed = this.mwrap.duration/(text.length*1.0) - this.mwrap.offset;
+		this.maxSpeed = this.minSpeed + this.mwrap.offset;
+	}
+}
 
-    }else{
-    for(var i = 0;i<this.Elements.length ;i++){
-       console.log(this.cloneWrap(this.Elements[i],this));
-       this.Elements[i].setWrap();
-    }}
-    return this.Elements;
-};
-initWrap.prototype.cloneWrap = function(c1,c2){
-    c1.cursorHeight = c2.cursorHeight;
-    c1.blinkFrequency = c2.blinkFrequency;
-    c1.dir = c2.dir;
-    c1.minSpeed = c2.minSpeed;
-    c1.maxSpeed = c2.maxSpeed;
-    c1.breakPoint = c2.breakPoint;
-    c1.breakTime = c2.breakTime;
-    c1.breakKey = c2.breakKey;
-    c1.wrapTextColor = c2.wrapTextColor;
-    c1.CursorBackgroundColor = c2.CursorBackgroundColor;
-    c1.wrapFontFamily = c2.wrapFontFamily;
-    c1.cursorWidth = c2.cursorWidth;
-    c1.wrapTextSize = c2.wrapTextSize;
-    c1.cursor.style.position = "absolute";
-    if(c1.wrapTextSize == null){
-       // c1.wrapTextSize = c1.getBoundingClientRect().height;
-    }
-    if(c1.cursorHeight == null){
-        c1.cursorHeight = c2.wrapTextSize;
-    }
-    return c1;
+function avcWrapAll()
+{
+	wrap = new avcWrap();
+	wrap.root = document.querySelector(":root");
+	wrap.elems = document.getElementsByClassName("texter");
+	wrap.wraps = [];
+	for(i = 0;i < wrap.elems.length;i++)
+	{
+		m = new avcTextWrap(wrap.elems[i]);
+		m.mwrap = wrap;
+		wrap.wraps[i] = m;
+		m.wrap();
+	}
+	return wrap;
 }
-initWrap.prototype.blinkCursor = function(This){
-    if (This.m > 0){
-        This.cursor.style.opacity = "0";
-        This.m = 0;
-        This.blinker = setTimeout(This.blinkCursor,This.blinkFrequency,This);
-    }else{
-        This.cursor.style.opacity = "1";
-        This.m = 1;
-        This.blinker = setTimeout(This.blinkCursor,This.blinkFrequency,This);
-    }
-};
-initWrap.prototype.getRandom = function(start,end){
-    return Math.floor(Math.random()*(end-start+1)) + start;
-};
-initWrap.prototype.setTextAbamTech = function(This){
-    This.elem.innerHTML = This.elem.textContent + '<span id="lastKeyAbam'+This.multiI+'" style="margin-right:20px;">'+This.content[This.i]+"</span>";
-    px = document.getElementById("lastKeyAbam"+This.multiI);
-    
-    This.cursor.style.top = px.getBoundingClientRect().top  + "px";
-    This.cursor.style.left = px.getBoundingClientRect().left + px.getBoundingClientRect().width + "px";
-    This.i++;
-    
-    if (This.i < This.content.length){
-        This.counter = This.getRandom(This.minSpeed,This.maxSpeed);
-        if((This.breakPoint != null && i == breakPoint) || (This.breakKey != null && This.content[i - 1] == This.breakKey)){
-            setTimeout(This.setTextAbamTech,This.breakTime,This);
-        }
-        else{
-            setTimeout(This.setTextAbamTech,This.counter,This);
-        }
-    }else{
-        finished = true;
-    }
-};
+
+function avcWrapOne(elem = null)
+{
+	wrap = new avcWrap();
+	wrap.root = document.querySelector(":root");
+	if(elem == null){
+		try{
+			wrap.elems = document.getElementsByClassName("texter");
+		}
+		catch(e){
+			console.log("No element with classname");
+			return null;
+		}
+	}
+	else{
+		if (typeof(elem) == 'number'){
+			try{
+				wrap.elems = [document.getElementsByClassName("texter")[elem]];
+				
+			}
+			catch(e){
+				console.log("No element found");
+				return null;
+			}
+		} 
+		else{
+			wrap.elems = [elem];
+		}
+	}
+	wrap.wraps = [];
+	m = new avcTextWrap(wrap.elems[0]);
+	m.mwrap = wrap;
+	wrap.wraps[0] = m;
+	m.wrap();
+	return wrap;
+}
